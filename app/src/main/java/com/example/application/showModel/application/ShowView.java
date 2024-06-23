f63846d37d827538ea7cc558f7b29df729ff5ea3
Hang <A18319521340@gmail.com>
2024-06-23 16:08:16 +0800

    完成到8

--- app/src/main/java/com/example/application/showModel/application/ShowView.java
+++ app/src/main/java/com/example/application/showModel/application/ShowView.java
@@ -2,6 +2,7 @@ package com.example.application.showModel.application;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -28,32 +29,36 @@ public class ShowView extends AppCompatActivity implements View.OnClickListener 
     private RecyclerView recyclerView;
     MyAdapter myAdapter;
 
-
     private static Activity mContext;
     private View mCurrentView;
 
     private EditText tv_title;
     private ImageButton ib_search;
     private LayoutInflater layoutInflater;
-    private List<FaBuBean> lists=new ArrayList<FaBuBean>();
+    private List<FaBuBean> lists = new ArrayList<>();
 
-    private FaBuSQLHelper sqlHelper=new FaBuSQLHelper();
+    private FaBuSQLHelper sqlHelper = new FaBuSQLHelper();
 
-    public ShowView(Activity context){
-        mContext=context;
-        layoutInflater=LayoutInflater.from(mContext);
+    public ShowView(Activity context) {
+        mContext = context;
+        layoutInflater = LayoutInflater.from(mContext);
     }
 
-    private void initView(){
-        mCurrentView=layoutInflater.inflate(R.layout.activity_show,null);
-        recyclerView=mCurrentView.findViewById(R.id.recyclerview);
-        ib_search=mCurrentView.findViewById(R.id.search);
-        tv_title=mCurrentView.findViewById(R.id.et_search_title);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getView());
+    }
+
+    private void initView() {
+        mCurrentView = layoutInflater.inflate(R.layout.activity_show, null);
+        recyclerView = mCurrentView.findViewById(R.id.recyclerview);
+        ib_search = mCurrentView.findViewById(R.id.search);
+        tv_title = mCurrentView.findViewById(R.id.et_search_title);
         List<FaBuBean> faBuBeans = sqlHelper.queryFaBuInfo(mContext, "");
-        for (FaBuBean bean: faBuBeans) {
-            lists.add(bean);
-        }
-        myAdapter=new MyAdapter();
+        lists.addAll(faBuBeans);
+
+        myAdapter = new MyAdapter();
         LinearLayoutManager layoutManager = new LinearLayoutManager(mContext);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(myAdapter);
@@ -61,14 +66,12 @@ public class ShowView extends AppCompatActivity implements View.OnClickListener 
         ib_search.setOnClickListener(this);
 
         mCurrentView.setVisibility(View.VISIBLE);
-
-
     }
 
     @Override
     public void onClick(View v) {
-        int id=v.getId();
-        if (id==R.id.search){
+        int id = v.getId();
+        if (id == R.id.search) {
             String title = tv_title.getText().toString();
             selectList(title);
         }
@@ -77,42 +80,38 @@ public class ShowView extends AppCompatActivity implements View.OnClickListener 
     private void selectList(String title) {
         List<FaBuBean> newDataLists = sqlHelper.queryFaBuInfo(mContext, title);
         myAdapter.updateDataList(newDataLists);
-
     }
 
-    public View getView(){
-        if (mCurrentView==null){
+    public View getView() {
+        if (mCurrentView == null) {
             initView();
         }
         return mCurrentView;
     }
 
-    public void showView(){
-        if (mCurrentView==null){
+    public void showView() {
+        if (mCurrentView == null) {
             initView();
-        }
-        else {
+        } else {
             List<FaBuBean> newDataLists = sqlHelper.queryFaBuInfo(mContext, "");
             myAdapter.updateDataList(newDataLists);
             mCurrentView.setVisibility(View.VISIBLE);
         }
-
     }
 
-
-    class MyAdapter extends RecyclerView.Adapter<MyViewHolder>{
+    class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
 
         @NonNull
         @Override
         public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            View view=View.inflate(mContext,R.layout.item_list,null);
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list, parent, false);
             MyViewHolder holder = new MyViewHolder(view);
             view.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     int adapterPosition = holder.getAdapterPosition();
                     Intent intent = new Intent(mContext, DetailActivity.class);
-                    intent.putExtra("EXTRA_DATA",lists.get(adapterPosition).get_id());
+                    intent.putExtra("EXTRA_DATA", lists.get(adapterPosition).get_id());
                     mContext.startActivity(intent);
                 }
             });
@@ -124,16 +123,14 @@ public class ShowView extends AppCompatActivity implements View.OnClickListener 
             FaBuBean faBuBean = lists.get(position);
             holder.mTitleTv.setText(faBuBean.getTitle());
             holder.mAuthorTv.setText(faBuBean.getKeys());
-//            holder.mAuthorTv.setText(sqlHelper.getUserName(mContext,faBuBean.getUserId()));
         }
-
 
         @Override
         public int getItemCount() {
-           return lists.size();
+            return lists.size();
         }
 
-        public void updateDataList(List<FaBuBean> newDataList){
+        public void updateDataList(List<FaBuBean> newDataList) {
             DiffUtil.DiffResult diffResult = DiffUtil.calculateDiff(new MyDiffCallback(lists, newDataList));
             lists.clear();
             lists.addAll(newDataList);
@@ -141,15 +138,14 @@ public class ShowView extends AppCompatActivity implements View.OnClickListener 
         }
     }
 
-
-    class MyViewHolder extends RecyclerView.ViewHolder{
+    class MyViewHolder extends RecyclerView.ViewHolder {
         private TextView mTitleTv;
         private TextView mAuthorTv;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
-            mTitleTv=itemView.findViewById(R.id.tv_title);
-            mAuthorTv=itemView.findViewById(R.id.tv_author);
+            mTitleTv = itemView.findViewById(R.id.tv_title);
+            mAuthorTv = itemView.findViewById(R.id.tv_author);
         }
     }
 }
